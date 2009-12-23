e5826234d7410b9e7b2b79c77ad38d5cf09b72e7
douglassquirrel <squirrel@blindpages.com>
2009-12-23 14:04:30 +0000

    Try defining mysql class

--- manifests/mysql.pp
+++ manifests/mysql.pp
@@ -1,3 +1,21 @@
+class mysql {
+
+  package{ [mysql-client, mysql-server, libmysqlclient15-dev]: ensure => installed }
+  
+  if $mysql_root_password { 
+        
+        exec { "Set MySQL server root password":
+          unless => "mysqladmin -uroot -p$mysql_root_password status",
+          path => "/bin:/usr/bin",
+          command => "mysqladmin -uroot password $mysql_root_password",
+          require => [ Package["mysql-server"], Package["mysql-client"] ]
+        }
+    }
+    else {
+        warning("no mysql root password given - sorry")
+    }
+  }
+
 define mysql::database($dbname, $ensure) {
 
     case $ensure {
