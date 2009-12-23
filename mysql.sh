6cdb96891df6196c24625c51976ae082529229f4
squirrel <squirrel@blindpagestest.Belkin>
2009-12-23 01:56:58 +0000

    Make mysql.sh re-entrant

--- mysql.sh
+++ mysql.sh
@@ -1,3 +1,4 @@
+echo "GRANT USAGE ON *.* TO 'blindpages'@'localhost'; DROP USER 'blindpages'@'localhost'" | mysql -uroot
 echo "CREATE USER 'blindpages'@'localhost' IDENTIFIED BY '$2'" | mysql -uroot
 echo "GRANT ALTER, CREATE, DELETE, DROP, INDEX, INSERT, SELECT, UPDATE ON blindpages.* TO 'blindpages'@'localhost'" | mysql -uroot
 mysqladmin -uroot password $1
