7a5cc451473492420fa8df493eaf43508cf1a5de
douglassquirrel <squirrel@blindpages.com>
2009-12-23 03:55:59 +0000

    Better way to test basic recipe

--- puppet.sh
+++ puppet.sh
@@ -7,16 +7,14 @@ puppet puppet-min.pp
 echo "BLINDPAGES: Test complete (should have printed 'hello world')"
 
 echo "BLINDPAGES: Copying files"
-cp sudo.pp /etc/puppet/manifests/classes
-cp site.pp /etc/puppet/manifests
+sudo mkdir /etc/puppet/manifests
+sudo mkdir /etc/puppet/manifests/classes
+sudo cp sudo.pp /etc/puppet/manifests/classes
+sudo cp site.pp /etc/puppet/manifests
 echo "BLINDPAGES: Files copied"
 
-echo "BLINDPAGES: Starting puppetmaster"
-sudo /etc/init.d/puppetmaster restart
-echo "BLINDPAGES: puppetmaster started"
-
-echo "BLINDPAGES: Running client"
-sudo puppetd --verbose
-sudo puppetca --list
-sudo puppetca --sign foo
-echo "BLINDPAGES: Client finished"
+echo "BLINDPAGES: Running puppet basic recipe"
+cd etc/puppet && puppet --parseonly manifests/site.pp
+cd etc/puppet && puppet --noop manifests/site.pp   --debug
+cd etc/puppet && puppet manifests/site.pp
+echo "BLINDPAGES: Puppet basic recipe finished"
