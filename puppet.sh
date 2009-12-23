cb3840ab2b9c93a635fe1af7f9ef620a3236b770
douglassquirrel <squirrel@blindpages.com>
2009-12-23 04:15:43 +0000

    Remove unwanted sudo

--- puppet.sh
+++ puppet.sh
@@ -11,8 +11,8 @@ sudo mkdir /etc/puppet/manifests
 sudo mkdir /etc/puppet/manifests/classes
 sudo cp sudo.pp /etc/puppet/manifests/classes
 sudo cp site.pp /etc/puppet/manifests
-sudo mkdir ~/.puppet
-sudo mkdir ~/.puppet/var
+mkdir ~/.puppet
+mkdir ~/.puppet/var
 echo "BLINDPAGES: Files copied"
 
 echo "BLINDPAGES: Running puppet basic recipe"
