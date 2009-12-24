f3e88c08a15ffc12038a053d990f5612913eff2e
douglassquirrel <squirrel@blindpages.com>
2009-12-24 19:08:28 +0000

    Try apache again without import

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,8 +1,6 @@
 node default {
-    include apache
     apache::site {"blindpages.com":}
 
     $mysql_root_password = ""
-    include mysql
     mysql::database{"buildbase_production": dbname => 'buildbase_production', ensure => present}
 }
