252890ba92a2d60d076ad3eae7a50de93bd82216
douglassquirrel <squirrel@blindpages.com>
2009-12-25 02:20:37 +0000

    Comment out apache

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,5 +1,5 @@
 node default {
-    apache::site {"blindpages.com":}
+#    apache::site {"blindpages.com":}
 
     $mysql_root_password = ""
     mysql::database{"buildbase_production": dbname => 'buildbase_production', ensure => present}
