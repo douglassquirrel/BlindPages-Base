c504d7af57c3b97fe8ff2957b594565e824d6293
douglassquirrel <squirrel@blindpages.com>
2009-12-25 02:27:35 +0000

    Try adding back the missing include statement

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,6 +1,7 @@
 node default {
 #    apache::site {"blindpages.com":}
 
+    include mysql
     $mysql_root_password = ""
     mysql::database{"buildbase_production": dbname => 'buildbase_production', ensure => present}
 }
