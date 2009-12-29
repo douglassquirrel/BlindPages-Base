1e954f7bea211c7755173209c1fe1598761dc117
douglassquirrel <squirrel@blindpages.com>
2009-12-29 11:19:21 +0000

    quote variable correctly

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,7 @@
 cat init.sql | while read sql
 do
   echo "Executing $sql"
-  echo $sql | mysql -uroot -pchangeme314159
+  echo "$sql" | mysql -uroot -pchangeme314159
 done
 
 mysqladmin -uroot -pchangeme314159 password $1
