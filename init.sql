e63b8d1624589a4cc347087729c726c5f8e5532c
douglassquirrel <squirrel@blindpages.com>
2009-12-29 11:26:56 +0000

    Move sql back into mysql.sh

--- init.sql
+++
@@ -1,4 +0,0 @@
-GRANT USAGE ON *.* TO 'blindpages'@'localhost'
-DROP USER 'blindpages'@'localhost'
-CREATE USER 'blindpages'@'localhost' IDENTIFIED BY '$2'
-GRANT ALTER, CREATE, DELETE, DROP, INDEX, INSERT, SELECT, UPDATE ON blindpages.* TO 'blindpages'@'localhost' 
--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,11 @@
-cat init.sql | while read sql
-do
-  echo "Executing $sql"
-  echo "$sql" | mysql -uroot -pchangeme314159
-done
+function dosql {
+  echo "Executing $1"
+  echo "$1" | mysql -uroot -pchangeme314159
+}
+
+dosql "GRANT USAGE ON *.* TO 'blindpages'@'localhost'"
+dosql "DROP USER 'blindpages'@'localhost'"
+dosql "CREATE USER 'blindpages'@'localhost' IDENTIFIED BY '$2'"
+dosql "GRANT ALTER, CREATE, DELETE, DROP, INDEX, INSERT, SELECT, UPDATE ON blindpages.* TO 'blindpages'@'localhost'"
 
 mysqladmin -uroot -pchangeme314159 password $1
