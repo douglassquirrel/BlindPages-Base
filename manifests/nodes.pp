955177b1bdee6ee16bb29d446d123d5b70f18cdf
douglassquirrel <squirrel@blindpages.com>
2009-12-30 01:38:07 +0000

    Change order of includes

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,10 +1,10 @@
 node default {
+    include apache::passenger
+
     include apache2::install
     apache::site {"blindpages.com":}
 
     $mysql_root_password = "changeme314159"
     include mysql
     mysql::database{"blindpages": dbname => 'blindpages', ensure => present}
-
-    include apache::passenger
 }
