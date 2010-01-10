641e5f9debfbd08a3f36580cd8120dd82c9c7992
douglassquirrel <squirrel@blindpages.com>
2010-01-10 12:15:23 +0000

    Install facter and puppet only if not already present

--- puppet.sh
+++ puppet.sh
@@ -26,25 +26,29 @@ read -p "Please enter desired MySQL root password: " rootpassword
 read -p "Please enter desired MySQL blindpages password: " blindpagespassword
 echo "BLINDPAGES: Done with MySQL setup"
 
-echo "BLINDPAGES: Downloading and unpacking facter and puppet"
-wget http://reductivelabs.com/downloads/puppet/puppet-0.24.8.tgz
-gzip -d -c puppet-0.24.8.tgz | tar xfm -
-wget http://reductivelabs.com/downloads/facter/facter-1.5.4.tgz
-gzip -d -c facter-1.5.4.tgz | tar xfm -
-echo "BLINDPAGES: Done downloading and unpacking facter and puppet"
+if type -P puppet &>/dev/null && type -P facter &>/dev/null; then
+    echo "BLINDPAGES: Facter and puppet already installed"
+else
+    echo "BLINDPAGES: Downloading and unpacking facter and puppet"
+    wget http://reductivelabs.com/downloads/puppet/puppet-0.24.8.tgz
+    gzip -d -c puppet-0.24.8.tgz | tar xfm -
+    wget http://reductivelabs.com/downloads/facter/facter-1.5.4.tgz
+    gzip -d -c facter-1.5.4.tgz | tar xfm -
+    echo "BLINDPAGES: Done downloading and unpacking facter and puppet"
 
-echo "BLINDPAGES: Installing facter"
-cd facter-*
-ruby install.rb
-cd ..
-echo "BLINDPAGES: Done installing facter"
+    echo "BLINDPAGES: Installing facter"
+    cd facter-*
+    ruby install.rb
+    cd ..
+    echo "BLINDPAGES: Done installing facter"
 
-echo "BLINDPAGES: Installing puppet"
-#apt-get -y install puppet
-cd puppet-*
-ruby install.rb
-cd ..
-echo "BLINDPAGES: Done installing puppet"
+    echo "BLINDPAGES: Installing puppet"
+    #apt-get -y install puppet
+    cd puppet-*
+    ruby install.rb
+    cd ..
+    echo "BLINDPAGES: Done installing puppet"
+fi
 
 echo "BLINDPAGES: Testing puppet"
 puppet puppet_min.pp
